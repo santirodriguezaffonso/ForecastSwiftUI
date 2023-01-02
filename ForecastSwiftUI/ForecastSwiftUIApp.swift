7238e8b4308aacacd1d86121f1a6281d436878ce
Santiago Rodriguez Affonso <sraffonso@itcrowdarg.com>
2023-01-01 22:09:34 -0300

    Initial Commit

---
+++ ForecastSwiftUI/ForecastSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastSwiftUIApp.swift
+//  ForecastSwiftUI
+//
+//  Created by Santiago Rodriguez Affonso on 01/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ForecastSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
